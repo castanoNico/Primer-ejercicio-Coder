72126baf8e355808cc462d3f5658da3659f23934
Nicolas Castaño <castano.nico@gmail.com>
2022-07-03 16:22:41 -0300

    cambio 2

---
+++ JS/clase.js
@@ -0,0 +1,82 @@
+// let numeroA = 1;
+// let numeroB = 2;
+// const numeroC = 3;
+
+// numeroA + numeroB;
+// se suman las variables
+
+// let resultadoSuma = numeroA + numeroB 
+
+
+
+
+
+
+
+
+//Pedir nombre mediante prompt y mostrarlo en consola junto con algún texto de saludo. Ejemplo:  ¡Hola, Juan!
+
+//creacion de variable
+//let nombreVisitante 
+
+//asignacion de valor a la variable
+ //nombreVisitante = prompt("Hola, ingrese su nombre")
+
+ // alert hace aparecer un cuadro de texto con el valor asignado de una variable, o un valor cualquiera especificado entre los paréntesis.
+//alert( "Hola " + nombreVisitante)
+
+
+// let nombreCliente 
+// nombreCliente = prompt ("hola, Ingresa tu nombre por favor")
+// console.log("que tal " + nombreCliente + " a continuacion te mostraremos los pasos a seguir brother")
+
+
+
+
+
+
+
+
+
+// Pedir un número mediante prompt, parsearlo, sumarlo a otro que se encuentre almacenado en una variable
+// y luego mostrar el resultado en consola.
+// let numeroPizas
+// alert("Comprandonos 1 Pizza o mas, te regalamos 1 pizza")
+// numeroPizas = prompt("cuantas pizas queres?")
+// numeroPizas = parseInt(numeroPizas)
+// let pizaRegalo
+// pizaRegalo = 1
+// console.log("Gracias por tu compra de " + (numeroPizas + pizaRegalo) + " Pizzas")
+
+
+
+
+
+
+
+//Pedir un texto mediante prompt, luego otro, concatenarlos y mostrarlo en un alerta.
+
+
+// let bandaFavorita 
+// bandaFavorita = prompt("Cual es tu banda favorita?")
+// let clubHincha
+// clubHincha = prompt("De que club sos hincha?")
+// alert ("tu banda favorita es" + " " + bandaFavorita + " " + "y sos hincha de" + " " + clubHincha)
+
+
+
+// JUNTAR EJERCICIO 1 Y 2 PARA HACER UNO MAS LARGO.
+
+
+
+let nombreComprador;
+nombreComprador = prompt("Hola, ¿como es tu nombre?");
+alert("Bienvenido al Emporio de la Pizza");
+let pedidoPiza;
+alert("Te recodarmos que con tu compra, te regalamos 1 pizza");
+pedidoPiza = prompt("Hola " + nombreComprador + " . Cuantas Pizzas queres?");
+pedidoPiza = parseInt (pedidoPiza)
+let pizaRegalo = 1;
+pedidoTotal = pedidoPiza + pizaRegalo
+alert("Tu pedido de " + pedidoTotal + " pizzas, esta siendo preparado! Gracias.")
+
