2e0c749c1bec343bc473a8feef7d356c736d273f
Nicolas Castaño <castano.nico@gmail.com>
2022-07-06 20:31:22 -0300

    actualizacion de css + img cargadas

---
+++ Clase 2/JS/script.js
@@ -0,0 +1,25 @@
+let edad = parseInt(prompt("Ingrese su edad:"))
+
+if(edad >= 18) {
+    alert("sos Mayor")
+}
+else{
+    alert("no sos mayor")
+}
+
+
+let precio = parseInt(prompt("Ingrese el precio:"))
+
+if(precio < 20) {
+    alert("el precio es menor que 20")
+}
+else if(precio < 50) {
+    alert("el precio es menor que 50")
+}
+else if(precio < 100) {
+    alert("el precio es menor que 100")
+}
+
+else{
+    alert("el precio es mayor que 100")
+}
