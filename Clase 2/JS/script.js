57d11d233fd8cb50d0662bf5dff0b7b8f3b9cb6a
Nicolas Castaño <castano.nico@gmail.com>
2022-07-10 18:27:36 -0300

    Commit 10/07

--- Clase 2/JS/script.js
+++ Clase 2/JS/script.js
@@ -1,25 +1,100 @@
-let edad = parseInt(prompt("Ingrese su edad:"))
+// let nombreProductoA = "mesa" //si no esta nombreProductoA funciona igual
+// let precioProductoA = 1000      
+// let stockProductoA = 10 
 
-if(edad >= 18) {
-    alert("sos Mayor")
+// let nombreProductoB = "silla"
+// let precioProductoB = 500
+// let stockProductoB = 100
+
+// let compra1 = prompt("Ingrese nombre del producto que quiere comprar: \n- mesa\n- silla")
+// let cantidad1 = prompt("ingrese la cantidad del producto que quiere comprar: ")
+
+// if (compra1 == "mesa") {
+//     if (stockProductoA >= cantidad1){
+//         alert("el precio es de: $ " + (cantidad1 * precioProductoA))
+//     }
+//     else{
+//         alert("No disponemos de esa cantidad en stock. Nuestro stock actual es de:" + stockProductoA)
+//     }
+// }
+// else if (compra1 == "Silla"){
+//     if (stockProductoB >= cantidad1){
+//         alert("el precio es de: $ " + (cantidad1 * precioProductoB))
+//     }
+//     else{
+//         alert("No disponemos de esa cantidad en stock. Nuestro stock actual es de:" + stockProductoA)
+//     }
+// }
+// else{
+//     alert("no tenemos ese producto")
+// }
+
+
+
+let entradaShowMayores = "Entrada para Mayores";
+let precioMayores = 800;
+let stockEntradasMayores = 25;
+
+let entradaShowMenores = "Entrada para Menores";
+let precioMenores = 400;
+let stockEntradasMenores = 25;
+
+alert("Bienvenido a la preventa de entradas para el Show del codigo")
+let compraEntrada = prompt("Eliga el tipo de entrada que quiere comprar:\n -Entrada para mayores \n -Entrada para menores \n -Ambas");
+
+let precioTotal = 0;
+let ambas = 2
+
+if (compraEntrada == "Entrada para mayores" || compraEntrada == "entrada para mayores" ) {
+    let cantidadEntradaMayores = prompt("Cuantas entradas para mayores quiere?"); 
+        if(stockEntradasMayores > cantidadEntradaMayores ){
+             alert("El precio es de: " +  (precioMayores * cantidadEntradaMayores));
+}
+        else{
+             alert("No tenemos esa cantidad de entradas");
+}
+
+}
+else if(compraEntrada == "Entrada para menores" || compraEntrada == "entrada para menores"){
+        let cantidadEntradaMenores = prompt("Cuantas entradas para menores quiere?");
+    if( stockEntradasMenores > cantidadEntradaMenores){
+        alert("El precio es de: " + (precioMenores * cantidadEntradaMenores));
+    }
+    else{
+        alert("no tenemos esa cantidad de entradas.");
+    }
+
+}
+
+else if(compraEntrada == "Ambas" || compraEntrada == "ambas") {
+    for(let i = 0; i < ambas; i++){
+        let compraEntrada = prompt("Eliga el tipo de entrada que quiere comprar:\n -Entrada para mayores \n -Entrada para menores")
+        if (compraEntrada == "Entrada para mayores" || compraEntrada == "entrada para mayores" ){
+            let cantidadEntradaMayores = prompt("Cuantas entradas para mayores quiere?"); 
+                if(stockEntradasMayores >= cantidadEntradaMayores ){
+                     alert("Usted compro: " +  cantidadEntradaMayores + " entradas");
+                     precioTotal += precioMayores * cantidadEntradaMayores;
+        }
+                else{
+                     alert("No tenemos esa cantidad de entradas.");
+        }
+        
+        }
+        else if(compraEntrada == "Entrada para menores" || compraEntrada == "entrada para menores"){
+            let cantidadEntradaMenores = prompt("Cuantas entradas para menores quiere?");
+            if (stockEntradasMenores >= cantidadEntradaMenores)
+            alert("Usted compro " + cantidadEntradaMenores + " entradas");
+            precioTotal += precioMenores * cantidadEntradaMenores;
+        }
+
+        else {
+            alert("No tenemos esa cantidad de entradas.");
+        }
+
+    }
+    alert("El precio total es de " + precioTotal + " pesos.");
 }
 else{
-    alert("no sos mayor")
+    alert("selecione una opcion correcta");
 }
 
-
-let precio = parseInt(prompt("Ingrese el precio:"))
-
-if(precio < 20) {
-    alert("el precio es menor que 20")
-}
-else if(precio < 50) {
-    alert("el precio es menor que 50")
-}
-else if(precio < 100) {
-    alert("el precio es menor que 100")
-}
-
-else{
-    alert("el precio es mayor que 100")
-}
---
+++ Clase 3/JS/script.js
@@ -0,0 +1,64 @@
+    //   cuantas veces se tiene que repetir  
+// for (let i = 0 ; i < 5 ; i = i + 1) { 
+//     console.log(i)
+//     console.log("Bienvenido")
+// }
+
+// let nombreIngresado = prompt("Ingrese su Nombre:")
+
+
+//DECLARADO
+
+// for (let i = 1 ; i < 3 ; i++){
+
+//     if(i == 2){
+//         alert("apurate quedan pocos turnos")
+//     }
+//     let nombreIngresado = prompt("Ingrese su Nombre:")
+//     alert("Bienvenido " + nombreIngresado + "." + "\nSu turno es el N: " + i)
+// }
+// alert("nosquedamos sin turnos")
+
+
+
+// CONTINUE: saltea la variable declarada en el IF
+// for (let i = 1 ; i < 7 ; i++){
+
+//     if(i == 4){
+//        continue;
+//     }
+//     console.log(i)
+// }
+
+
+//BREAK: corta el ciclo en el numero que declaremos en el IF
+// for (let i = 1 ; i < 7 ; i++){
+
+//     if(i == 4){
+//        break;
+//     }
+//     console.log(i)
+// }
+
+
+
+
+//EJEMPLO
+// let nombreIngresado = ""
+
+// let numero = 20
+
+// for ( let i = 0 ; i <= 20 ; i++){
+
+//     if(i == 2 || i == 4){
+//         continue;
+//     }    
+//     if(i == 15){
+//         break;
+//     }
+
+//     console.log(i)
+// }
+
+
+
